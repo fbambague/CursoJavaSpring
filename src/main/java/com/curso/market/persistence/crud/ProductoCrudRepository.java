ee365ad8de6265ce3c1651b88779f73d0cbc71fc
fbambague <dbambague7@gmail.com>
2022-08-22 18:13:20 -0500

    Producto repository

--- src/main/java/com/curso/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/curso/market/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,11 @@ package com.curso.market.persistence.crud;
 import com.curso.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    public List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    public Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidad, boolean estado);
+
 }
